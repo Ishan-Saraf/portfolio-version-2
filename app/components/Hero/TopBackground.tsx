dd5fffb3ce5ade0818d9a8e58b4a86c29cc97ab5
Ishan-Saraf <ishaansaraf03@gmail.com>
2025-03-03 22:56:32 +0530

    Fixed typo in TopBackground.tsx and personal.json

--- app/components/Hero/TopBackground.tsx
+++ app/components/Hero/TopBackground.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function TopBackground() {
   return (
     <>
-      <div className="absolute w-full max-w-[360px] md:max-w-screen-lg h-[256px] md:h-[656] top-[188px] md:top-11 left-1/2 -translate-x-1/2 md:overflow-x-hidden z-10">
+      <div className="absolute w-full max-w-[360px] md:max-w-screen-lg h-[256px] md:h-[656px] top-[188px] md:top-11 left-1/2 -translate-x-1/2 md:overflow-x-hidden z-10">
         <div className="w-full h-full relative">
           <div className="circle-icon top-0 left-[171px] md:top-28 md:left-72">
             <Image src="/rocket_icon.svg" alt="rocket" width={23} height={23} />
